a79de36d2b7addfd3929908d1b25e85b557ae8fd
ZhangZhenjie <zhangzhenjie@gmail.com>
2021-08-23 11:28:04 +0800

    Add kiasuparent scrapy codes.

---
+++ Scrapy/kiasuparent/kiasuparent/pipelines.py
@@ -0,0 +1,28 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+
+class KiasuparentPipeline:
+    def __init__(self):
+        connection = pymongo.MongoClient(
+            settings['MONGODB_SERVER'],
+            settings['MONGODB_PORT']
+        )
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['POST_COLLECTION']]
+
+    def process_item(self, item, spider):
+        valid = True
+        for data in item:
+            if not data:
+                valid = False
+                raise DropItem("Missing {0}!".format(data))
+        if valid:
+            self.collection.insert(dict(item))
+        return item
